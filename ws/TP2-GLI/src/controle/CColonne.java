946d1e074e36eff3ee8af34cacf436038997a0c1
Thomas Lelievre <thomaslelievre72@gmail.com>
2013-10-22 23:23:23 +0200

    Solitaire: pb du CColonne.

---
+++ ws/TP2-GLI/src/controle/CColonne.java
@@ -0,0 +1,13 @@
+package controle;
+
+import solitaire.application.Colonne;
+import solitaire.application.Usine;
+
+public class CColonne extends Colonne {
+
+	public CColonne(String arg0, Usine arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+}
